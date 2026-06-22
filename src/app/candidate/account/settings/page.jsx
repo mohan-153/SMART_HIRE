e29eb76c98a20b20cb3be2e6db2363b6a3264af9
mohan-153 <23ucs025@rmv.ac.in>
2026-06-22 13:04:33 +0530

    Fix candidate settings page

---
+++ src/app/candidate/account/settings/page.jsx
@@ -0,0 +1,8 @@
+export default function SettingsPage() {
+	return (
+		<div className="p-6">
+			<h1>Account Settings</h1>
+		</div>
+	);
+}
+
---
+++ src/app/recruiter/notifications/page.jsx
@@ -0,0 +1,8 @@
+export default function RecruiterNotificationsPage() {
+	return (
+		<div className="p-6">
+			<h1>Notifications</h1>
+		</div>
+	);
+}
+
